36a4ccea4364de2883a9e9712ad1a966e808a800
stephen.wille <stephen.wille@gmail.com>
2017-03-04 15:39:14 -0800

    Simple component

---
+++ app/events-app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector:'events-app',
+    template:`<h1>woot</h1>`
+})
+export class EventsAppComponent{
+
+}
--- systemjs.config.js
+++ systemjs.config.js
@@ -25,7 +25,7 @@
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
       // other libraries
-      'rxjs':                       'npm:rxjs',
+      'rxjs': 'npm:rxjs',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
